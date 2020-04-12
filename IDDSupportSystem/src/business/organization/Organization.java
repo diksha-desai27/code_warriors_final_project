91320e010c9592aa00c4045e48e654ac929effd5
Diksha Desai <desai.di@husky.neu.edu>
2020-04-11 23:07:59 -0400

    Design classes

---
+++ IDDSupportSystem/src/business/organization/Organization.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.organization;
+
+import business.role.Role;
+import business.useraccount.UserAccountDirectory;
+import business.workqueue.WorkQueue;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author dikshadesai
+ */
+public abstract class Organization {
+    
+    private String name;
+    private WorkQueue workQueue;
+   // private EmployeeDirectory employeeDirectory;
+    private UserAccountDirectory userAccountDirectory;
+    
+   public enum Type {
+    Analyst("Analyst Organization"), Doctor("Doctor Organization"),Caregiver("Caregiver Organization"), Admin("Admin Organization"), Nurse("NurseOrganization");
+
+    private String value;
+        
+    private Type(String value) {
+            this.value = value;
+        }
+        
+    public String getValue() {
+            return value;
+        }
+   }
+   
+    public Organization(String name) {
+        this.name = name;
+        this.workQueue = workQueue;
+        this.userAccountDirectory = userAccountDirectory;
+    }
+
+    public abstract ArrayList<Role> getSupportedRole();
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }   
+   
+    
+}
---
+++ IDDSupportSystem/src/business/role/NurseRole.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.role;
+
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.organization.Organization;
+import business.useraccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author dikshadesai
+ */
+public class NurseRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
