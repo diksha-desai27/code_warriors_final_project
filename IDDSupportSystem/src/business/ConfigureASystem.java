3e9ee4dca5c2636666065c2b7fee9cd307b3a09b
Diksha Desai <desai.di@husky.neu.edu>
2020-04-12 01:36:23 -0400

    Added DB40 util

---
+++ IDDSupportSystem/src/business/ConfigureASystem.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business;
+
+import business.role.SystemAdminRole;
+import business.useraccount.UserAccount;
+
+/**
+ *
+ * @author dikshadesai
+ */
+public class ConfigureASystem {
+    
+    public static EcoSystem configure() {
+        
+        EcoSystem system = EcoSystem.getInstance();
+        
+        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", new SystemAdminRole());
+        
+        return system;
+    }
+    
+}
