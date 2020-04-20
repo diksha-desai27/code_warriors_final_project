d5fce0233672eecd09023030c6444d51e4fca4b1
Sai Kale <kale.sa@husky.neu.edu>
2020-04-19 20:07:46 -0400

    doctor schedule changes

--- IDDSupportSystem/src/business/schedule/Schedule.java
+++ IDDSupportSystem/src/business/schedule/Schedule.java
@@ -6,6 +6,8 @@
 package business.schedule;
 
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -25,21 +27,23 @@ public class Schedule {
     public Schedule() {
         //   this.dateSchedule = ;
         Calendar c1 = Calendar.getInstance();
-        c1.add(Calendar.DATE, 1);
-        Date startDate = c1.getTime();
+        // c1.add(Calendar.DATE, 1);
+        // Date startDate = c1.getTime();
 
         Calendar c = Calendar.getInstance();
-        c.add(Calendar.DATE, 30);
-        Date endDate = c.getTime();
+        c.add(Calendar.DATE, 1);
 
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        String incDate = sdf.format(c.getTime());
         Calendar c2 = Calendar.getInstance();
         c2.add(Calendar.DATE, 1);
-    
-        
-        while (startDate.before(endDate)) {
-            //  List<String> timeList = new ArrayList<>();
-                    Date addDate = c2.getTime();
 
+        int maxDay = c1.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        for (int co = 0; co < maxDay; co++) {
+
+            c.add(Calendar.DATE, 1);
+            incDate = sdf.format(c.getTime());
             System.out.println("in for");
             HashMap<String, Boolean> slotSchedule = new HashMap<>();
             slotSchedule.put("9 am", true);
@@ -47,16 +51,17 @@ public class Schedule {
             slotSchedule.put("11 am", true);
             slotSchedule.put("5 pm", true);
             slotSchedule.put("6 pm", true);
-            System.out.println(startDate);
-            System.out.println(slotSchedule);
+            System.out.println(incDate);
             if (dateSchedule == null) {
                 dateSchedule = new HashMap<Date, HashMap<String, Boolean>>();
             }
+            try{
+            dateSchedule.put(sdf.parse(incDate), slotSchedule); //   this.dateSchedule.put(date,timeList);
+            }catch(ParseException e){
+                e.printStackTrace();
+            }
+            // c2.add(, 1);
 
-            dateSchedule.put(addDate, slotSchedule); //   this.dateSchedule.put(date,timeList);
-             c2.add(Calendar.DAY_OF_MONTH, 1);
-
-            
         }
 
         System.out.println(dateSchedule);
